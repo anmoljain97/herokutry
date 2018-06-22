e441f8604413651dad6f9d0ce3a71406098a0bc9
anmol jain <anmol97j@gmail.com>
2018-06-22 22:20:56 +0530

    final
    Signed-off-by: anmol jain <anmol97j@gmail.com>

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const path = require('path')
+
+const SERVER_PORT = process.env.PORT || 2678
+
+const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.use('/', express.static(path.join(__dirname, 'public')))
+app.use('/api', require('./routes/api').route)
+
+app.listen(SERVER_PORT, () => console.log('Server started at http://localhost:2678'))
